6e2e5131ce8317a88f28ef83cbeaa4db78f779ec
Philip-Walsh <philip.walsh@ericsson.com>
2024-05-27 14:35:31 +0100

    base template

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Go App
 
 Basic Web application built in Go
+
+## Build and run
+
+```bash
+go build . && ./go-app
+```
+
+## Run Dev mode
+
+```bash
+air
+```
--- main.go
+++ main.go
@@ -1,24 +1,35 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/joho/godotenv"
 )
 
+var tpl = template.Must(template.ParseFiles("index.html"))
+
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("<h1>Hello World!</h1>"))
+	tpl.Execute(w, nil)
 }
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Error loading .env file")
+	}
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "3000"
 	}
-
+	fileServer := http.FileServer(http.Dir("assets"))
+	// Router
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", indexHandler)
+	mux.Handle("/assets/", http.StripPrefix("/assets/", fileServer))
 	http.ListenAndServe(":"+port, mux)
-	log.Println(os.Stdout, "Listening on port ", port, "\n")
+	log.Println("Listening on port ", port)
 }
